7f76f82af7a187c8cba9f859cee11a9fc5e1dcfa
Sachin Kumar <sjun1992@gmail.com>
2018-08-12 20:40:02 +0530

    Fixed everything

--- src/main/webapp/resources/js/controllers/LoginController.js
+++ src/main/webapp/resources/js/controllers/LoginController.js
@@ -4,31 +4,6 @@ var app = angular.module('AutomatedAlgoApp.Login', []);
 
 app.controller('LoginController', function($rootScope, $scope, $location, CommonService, 
 		UserService) {
+	
 	$scope.header = 'Please login';
-	$scope.login = function() {
-		CommonService.auth = btoa($scope.user.userName + ":"
-				+ $scope.user.password);
-		UserService.login($scope.user).then(
-				function(response) {
-					if (response.data.userName != null) {
-						$rootScope.authenticated = true;
-						$rootScope.owner = response.data.userName;
-						$rootScope.admin = response.data.roles
-								.indexOf("ROLE_ADMIN") > -1;
-						$rootScope.user = response.data.roles
-								.indexOf("ROLE_USER") > -1;
-						$location.path('/home');
-					} else {
-						$scope.handleError();
-					}
-				}, function(error) {
-					$scope.error = true;
-					$rootScope.authenticated = false;
-					CommonService.auth = '';
-					$rootScope.admin = false;
-					$rootScope.user = false;
-					swal('Unable to login : ' + error.data.message);
-					$location.path('/');
-				});
-	};
 });
